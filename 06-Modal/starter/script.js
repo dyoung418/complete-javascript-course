477fd826aad2af2dd3378db44b6e8881db869e53
Danny Young <git@youngshome.com>
2021-06-20 23:58:36 -0700

    mid-way thru modal project

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsShowModal = document.querySelectorAll('.show-modal');
+
+btnsShowModal.forEach((button, index) =>
+  button.addEventListener('click', function () {
+    console.log(`hi from ${index}`);
+    console.log(` or from ${this.textContent}`);
+  })
+);
+
+console.log(btnsShowModal);
